744425a878416f83bbb7c1b05d65da1dd17aa3ff
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2025-11-17 22:16:55 -0500

    Add a Win64 UPX toolchain build.

--- taskcluster/scripts/misc/build-upx.sh
+++ taskcluster/scripts/misc/build-upx.sh
@@ -4,11 +4,25 @@ set -x -e -v
 # This script is for building UPX.
 PROJECT="upx"
 
+if test -d "$MOZ_FETCHES_DIR/cmake"; then
+    export PATH="$(cd $MOZ_FETCHES_DIR/cmake && pwd)/bin:${PATH}"
+fi
+
+if [[ $(uname -o) == "Msys" ]]; then
+  MAKE="$MOZ_FETCHES_DIR/mozmake/mozmake"
+  SUFFIX=".exe"
+  . "$GECKO_PATH/taskcluster/scripts/misc/vs-setup.sh"
+else
+  MAKE=make
+  SUFFIX=""
+fi
+
 pushd "${MOZ_FETCHES_DIR}/${PROJECT}"
-make
+$MAKE
 popd
 
-mv "${MOZ_FETCHES_DIR}/${PROJECT}/build/release/upx" "${PROJECT}"
+mkdir -p "${PROJECT}/bin"
+mv "${MOZ_FETCHES_DIR}/${PROJECT}/build/release/upx${SUFFIX}" "${PROJECT}/bin/"
 tar -acf "${PROJECT}.tar.zst" "${PROJECT}"
 
 mkdir -p "$UPLOAD_DIR"
