19767f4f7a8108ce86a992b936820cb94d5b85a2
Kagami Sascha Rosylight <krosylight@proton.me>
2026-02-03 20:32:53 +0000

    Bug 2013396 - Do not include geo.provider.network.* prefs in Android r=handyman
    But NetworkGeolocationProvider can't be removed as it's still needed to be served as a mock provider for WPT. We can try replacing it with a simpler one if we ever decide to fully remove NetworkGeolocationProvider, but doing so doesn't sound particularly useful not right now.
    
    Differential Revision: https://phabricator.services.mozilla.com/D281075

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3099,12 +3099,14 @@ pref("network.tcp.keepalive.idle_time", 600); // seconds; 10 mins
 
 // All the Geolocation preferences are here.
 //
-pref("geo.provider.network.url", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_LOCATION_SERVICE_API_KEY%&solution_channel=%OS%");
+#ifndef ANDROID
+  pref("geo.provider.network.url", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_LOCATION_SERVICE_API_KEY%&solution_channel=%OS%");
 
-// Timeout to wait before sending the location request.
-pref("geo.provider.network.timeToWaitBeforeSending", 5000);
-// Timeout for outbound network geolocation provider.
-pref("geo.provider.network.timeout", 60000);
+  // Timeout to wait before sending the location request.
+  pref("geo.provider.network.timeToWaitBeforeSending", 5000);
+  // Timeout for outbound network geolocation provider.
+  pref("geo.provider.network.timeout", 60000);
+#endif
 
 #ifdef XP_MACOSX
   pref("geo.provider.use_corelocation", true);
